135e6118600e7a3fd1169a73fb1d1baecfb9bdca
Aleksandr Musarov <musarov@cifra-broker.ru>
2025-03-07 15:00:15 +0300

    Added admin side and create product layout in admin side

---
+++ src/app/(admin)/menu/[id].tsx
@@ -0,0 +1,56 @@
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import React, { useState } from "react";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import products from "@/assets/data/products";
+import { defaultPizzaImage } from "@/src/components/ProductListItem";
+import Button from "@/src/components/Button";
+import { useCart } from "@/src/providers/CartProvider";
+import { PizzaSize } from "@/food-ordering-asset-bundle/Food Ordering Asset bundle/types";
+
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
+
+const ProductDetailsScreen = () => {
+  const { id } = useLocalSearchParams();
+  const product = products.find((product) => product.id.toString() === id);
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
+  const { addItem } = useCart();
+  const router = useRouter();
+
+  if (!product) {
+    return <Text>Product not found</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
+      <Text style={styles.title}>${product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
+
+export default ProductDetailsScreen;
---
+++ src/app/(user)/menu/index.tsx
@@ -0,0 +1,15 @@
+import { FlatList } from "react-native";
+import products from "@assets/data/products";
+import ProductListItem from "@components/ProductListItem";
+
+export default function MenuScreen() {
+  return (
+    <FlatList
+      data={products}
+      numColumns={2}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
+    />
+  );
+}

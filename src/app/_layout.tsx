7e94dcf6ba77b77f5e1ed1336b8927169361df4e
Aleksandr Musarov <musarov@cifra-broker.ru>
2025-03-21 14:11:32 +0300

    Basic push notifications

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -15,6 +15,7 @@ import CartProvider from "@providers/CartProvider";
 import AuthProvider from "@providers/AuthProvider";
 import QueryProvider from "@providers/QueryProvider";
 import { StripeProvider } from "@stripe/stripe-react-native";
+import NotificationsProvider from "../providers/NotificationsProvider";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -63,14 +64,28 @@ function RootLayoutNav() {
       >
         <AuthProvider>
           <QueryProvider>
-            <CartProvider>
-              <Stack>
-                <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-                <Stack.Screen name="(admin)" options={{ headerShown: false }} />
-                <Stack.Screen name="(user)" options={{ headerShown: false }} />
-                <Stack.Screen name="cart" options={{ presentation: "modal" }} />
-              </Stack>
-            </CartProvider>
+            <NotificationsProvider>
+              <CartProvider>
+                <Stack>
+                  <Stack.Screen
+                    name="(auth)"
+                    options={{ headerShown: false }}
+                  />
+                  <Stack.Screen
+                    name="(admin)"
+                    options={{ headerShown: false }}
+                  />
+                  <Stack.Screen
+                    name="(user)"
+                    options={{ headerShown: false }}
+                  />
+                  <Stack.Screen
+                    name="cart"
+                    options={{ presentation: "modal" }}
+                  />
+                </Stack>
+              </CartProvider>
+            </NotificationsProvider>
           </QueryProvider>
         </AuthProvider>
       </StripeProvider>
---
+++ src/providers/NotificationsProvider.ts
@@ -0,0 +1,54 @@
+import { PropsWithChildren, useEffect, useRef, useState } from "react";
+import { registerForPushNotificationsAsync } from "@lib/notifications";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+const NotificationsProvider = ({ children }: PropsWithChildren) => {
+  const [expoPushToken, setExpoPushToken] = useState<string | undefined>();
+  const [notification, setNotification] =
+    useState<Notifications.Notification>();
+  const notificationListener = useRef<Notifications.EventSubscription>();
+  const responseListener = useRef<Notifications.EventSubscription>();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      if (notificationListener.current) {
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+      }
+
+      if (responseListener.current) {
+        Notifications.removeNotificationSubscription(responseListener.current);
+      }
+    };
+  }, []);
+
+  console.info("Push token", expoPushToken);
+  console.info('NOTIFICATION', notification);
+
+  return children;
+};
+
+export default NotificationsProvider;
